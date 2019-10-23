ab8e1200db2a9b66c07e4cccc19d5d9345451400
Zephyr <brave_zephyr@163.com>
2019-10-23 18:36:27 +0800

    修正UnShorten的命名规则

--- storage.go
+++ storage.go
@@ -4,5 +4,5 @@ package main
 type Storage interface {
 	Shorten(url string, exp int64) (string, error)
 	ShortlinkInfo(eid string) (interface{}, error)
-	Unshorten(eid string) (string, error)
+	UnShorten(eid string) (string, error)
 }
