b8092336a1502c67ac09372a9d11842aa5caa6a1
Zephyr <brave_zephyr@163.com>
2019-10-23 18:39:39 +0800

    完成shorten方法

--- redis.go
+++ redis.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"crypto/sha1"
+	"encoding/json"
+	"fmt"
+	"github.com/catinello/base62"
 	"github.com/go-redis/redis"
 	"time"
 )
@@ -25,9 +29,9 @@ type RedisClient struct {
 
 // ShortlinkInfo short_link info
 type ShortlinkInfo struct {
-	URL                 string        `json:"url"`
-	CreatedAt           string        `json:"created_at"`
-	ExpirationInMinutes time.Duration `json:"expiration_in_minutes"`
+	URL                 string `json:"url"`
+	CreatedAt           string `json:"created_at"`
+	ExpirationInMinutes int64  `json:"expiration_in_minutes"`
 }
 
 func NewRedisCli(addr string, pass string, db int) *RedisClient {
@@ -40,4 +44,77 @@ func NewRedisCli(addr string, pass string, db int) *RedisClient {
 		panic("create redis client failed")
 	}
 	return &RedisClient{Cli: c}
+}
+
+func (cli *RedisClient) Shorten(url string, exp int64) (string, error) {
+	hashUrl := fmt.Sprintf("%s", sha1.Sum([]byte(url)))
+
+	d, err := cli.Cli.Get(hashUrl).Result()
+
+	if err == redis.Nil {
+		// url not exist, nothing to do
+	} else if err != nil {
+		return "", err
+	} else {
+		if d == "{}" {
+			// url expired, nothing to do
+		} else {
+			return d, nil
+		}
+	}
+
+	// Incr global counter
+	if err := cli.Cli.Incr(URLIDKEY).Err(); err != nil {
+		return "", err
+	}
+
+	// get global counter
+	urlId, err := cli.Cli.Get(URLIDKEY).Int()
+	if err != nil {
+		return "", err
+	}
+
+	// convert int to short link
+	eid := base62.Encode(urlId)
+
+	// set key fot short link to url
+	err = cli.Cli.Set(fmt.Sprintf(ShortlinkKey, eid), url, time.Minute*time.Duration(exp)).Err()
+	if err != nil {
+		return "", err
+	}
+
+	// set key for sha1(url) to short link
+	err = cli.Cli.Set(fmt.Sprintf(URLHashKey, hashUrl), eid, time.Minute*time.Duration(exp)).Err()
+	if err != nil {
+		return "", err
+	}
+
+	// set detail for short link
+	shortLinkInfo := &ShortlinkInfo{
+		URL:                 url,
+		ExpirationInMinutes: exp,
+		CreatedAt:           time.Now().String(),
+	}
+
+	// serialize short link info
+	jsonStr, err := json.Marshal(shortLinkInfo)
+	if err != nil {
+		return "", err
+	}
+
+	// set key for short link to detail
+	err = cli.Cli.Set(fmt.Sprintf(ShortlinkDetailKey, eid), jsonStr, time.Minute*time.Duration(exp)).Err()
+	if err != nil {
+		return "", err
+	}
+
+	return eid, err
+}
+
+func (cli *RedisClient) ShortlinkInfo(eid string) (interface{}, error) {
+	return nil, nil
+}
+
+func (cli *RedisClient) UnShorten(eid string) (string, error) {
+	return "", nil
 }
