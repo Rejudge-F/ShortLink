becc977baffb47c4340e504a78c9de5205554dc4
Zephyr <brave_zephyr@163.com>
2019-10-23 17:34:04 +0800

    添加env环境变量

---
+++ env.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"github.com/astaxie/beego/logs"
+	"os"
+	"strconv"
+)
+
+type Env struct {
+	storage Storage
+}
+
+func getEnv() *Env {
+	addr := os.Getenv("APP_REDIS_ADDR")
+	if addr == "" {
+		addr = "localhost"
+	}
+
+	pass := os.Getenv("APP_REDIS_PASSWD")
+	if pass == "" {
+		pass = ""
+	}
+
+	db := os.Getenv("APP_REDIS_DB")
+	if db == "" {
+		db = "0"
+	}
+	index, err := strconv.Atoi(db)
+	if err != nil {
+		logs.Debug("%s not db index", db)
+		panic(err)
+	}
+
+	cli := NewRedisCli(addr, pass, index)
+
+	return &Env{storage: cli}
+}
